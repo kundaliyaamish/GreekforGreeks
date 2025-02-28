da394b2138903cf8300864e6719a184a1e37cc8a
kundaliyaamish <128204462+kundaliyaamish@users.noreply.github.com>
2025-02-28 09:41:07 +0530

    Add files via upload

---
+++ Day_38_Search in a Row-Column sorted matrix.js
@@ -0,0 +1,92 @@
+//{ Driver Code Starts
+// Initial Template for javascript
+
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((string) => {
+      return string.trim();
+    });
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/* Function to print an array */
+function printArray(arr, size) {
+  let i;
+  let s = "";
+  for (i = 0; i < size; i++) {
+    if (arr[i] == -0) arr[i] = 0;
+    s += arr[i];
+    s += " ";
+  }
+  console.log(s);
+}
+
+function main() {
+  let t = parseInt(readLine());
+  let i = 0;
+  for (; i < t; i++) {
+    let n = parseInt(readLine());
+    let m = parseInt(readLine());
+    let matrix = new Array(n);
+    for (let i = 0; i < n; i++) {
+      let a = new Array(m);
+      let input_ar1 = readLine()
+        .split(" ")
+        .map((x) => parseInt(x));
+      for (let j = 0; j < m; j++) a[j] = input_ar1[j];
+      matrix[i] = a;
+    }
+    let X = parseInt(readLine());
+    let obj = new Solution();
+    let ans = obj.matSearch(matrix, X);
+    if (ans) console.log("true");
+    else console.log("false");
+    console.log("~");
+  }
+}
+// } Driver Code Ends
+
+// User function Template for javascript
+
+/**
+ * @param {number[][]} mat
+ * @param {number} x
+ * @returns {bool}
+ */
+class Solution {
+  matSearch(mat, x) {
+    // code here
+    let n = mat.length,
+      m = mat[0].length;
+    let i = n - 1;
+    let j = 0;
+    while (i >= 0 && j < m) {
+      if (x === mat[i][j]) return true;
+      else if (mat[i][j] < x) {
+        j = j + 1;
+      } else if (mat[i][j] > x) {
+        i = i - 1;
+      } else return false;
+    }
+    return false;
+  }
+}
